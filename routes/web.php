4f1793954b34da16d67d1f50213a41679d57354d
Harrison Kim <hsookim90@gmail.com>
2021-12-17 16:25:20 -0500

    Add cdn for fontawesome and use route from vue instead of laravel.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Required for Vue Router.
+Route::get('/{vue_capture?}', function() {
     return view('welcome');
-});
+})->where('vue_capture', '[\/\w\.-]*');
