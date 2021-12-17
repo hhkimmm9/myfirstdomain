f18503af68ea70f51ffaa309d68486da6c582b06
Harrison Kim <hsookim90@gmail.com>
2021-12-17 16:28:44 -0500

    Create the account component.

---
+++ resources/js/views/Account.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="Account">
+        <h1>Account</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Account",
+};
+</script>
+
+<style>
+#Account {
+    text-align: center;
+}
+</style>
