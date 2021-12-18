13c09985a27348ecfa168c83b9f3391ec9c0ed50
Harrison Kim <hsookim90@gmail.com>
2021-12-17 19:51:17 -0500

    Install Laravel Breeze.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+// Required for Vue Router.
+Route::get('/{vue_capture?}', function() {
     return view('welcome');
-});
+})->where('vue_capture', '[\/\w\.-]*');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
